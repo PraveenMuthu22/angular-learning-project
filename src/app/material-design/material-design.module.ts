b69d2c7c63113bc85fd20f29126a0b8a62205d30
Praveen Muthukumarana <praveen@skoolbag.com.au>
2021-02-24 10:52:57 +0530

    Created navigation and material design modules

---
+++ src/app/material-design/material-design.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialDesignRoutingModule } from './material-design-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialDesignRoutingModule
+  ]
+})
+export class MaterialDesignModule { }
